b26339ea41344f7d103d32d5869a6edcd7ccff3d
Senyo Mante <senyokwasimante@gmail.com>
2025-01-14 11:25:44 +0000

    14/01/2025

---
+++ R basics/Objects.R
@@ -0,0 +1,12 @@
+#Solve the quadratic equation of the form ax^2 + bx + c
+
+# assigning values to variables
+a <- 1
+b <- 1
+c <- -1
+
+print(c(a,b,c))
+ 
+#solving the quadratic equation
+(-b + sqrt(b^2 -4*a*c))/(2*a)
+(-b - sqrt(b^2 -4*a*c))/(2*a)
