2617605e985021af57e2318a97324f627d8416b5
Senyo Mante <senyokwasimante@gmail.com>
2025-01-14 12:33:05 +0000

    14/01/2025

---
+++ R basics/Functions.R
@@ -0,0 +1,52 @@
+#In-built functions 
+
+log(8)
+exp(1)
+
+#applying a function to a variable
+a <- 1
+log(a) 
+
+#Nested Functions
+log(exp(1))
+
+#help file in R
+?log
+
+#Arguments of a function
+args(log)
+
+#Changing default arguments
+log(8,base=2)
+
+log(4,4)
+
+#functions without parenthesis
+2^3    #arithmetic operations
+
+#Other Pre-built data Set & Objects
+data()
+
+pi
+
+Inf
+
+#Variable Naming conventions
+solution_1 <- (-b + sqrt(b^2 -4*a*c))/(2*a)
+solution_2 <- (-b - sqrt(b^2 -4*a*c))/(2*a)
+
+
+#Scripts
+a <- 1
+b <- 1
+c <-1
+
+solution_1
+solution_2
+
+a <- 3
+b <- 2
+c <- -1
+
+solution_1
+solution_2
